fcea81eccb272454b91c4a95b19bee1976b0c0a3
van huong <tovanhuong007@gmail.com>
2024-09-19 00:02:00 +0700

    Cau hinh ESLint cho du an

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {
+        // Thêm các quy tắc tùy chỉnh ở đây nếu cần
+    },
+};
